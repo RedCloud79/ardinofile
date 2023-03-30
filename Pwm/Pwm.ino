e155488a8eeeff2532dfde7cc05a6b63d48867f4
RedCloud79 <ghddns79@naver.com>
2023-03-30 14:38:33 +0900

    Update Pwm.ino

--- Pwm/Pwm.ino
+++ Pwm/Pwm.ino
@@ -4,7 +4,7 @@ const int LED = 10;
 
 void setup() {
   Timer1.initialize();
-  Timer.pwm(LED, 0);
+  Timer1.pwm(LED, 0);
 
   Timer1.setPeriod(1000000); // 1Hz
   Timer1.setPwmDuty(LED, 511); // 0~1023 LED에 대한 상하비
