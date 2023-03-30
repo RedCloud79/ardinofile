97063f13e52fc0cb0920e1e6977ee91a80899acc
RedCloud79 <ghddns79@naver.com>
2023-03-30 09:57:08 +0900

    arduino board led control

---
+++ digitalwrite/digitalwrite.ino
@@ -0,0 +1,13 @@
+const int Led = 13;
+
+void setup() {
+  pinMode(Led, OUTPUT);
+  digitalWrite(Led, HIGH);
+}
+
+void loop() {
+  // digitalWrite(Led, HIGH);
+  // delay(300);
+  // digitalWrite(Led, LOW);
+
+}
