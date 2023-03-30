eb4bd5b4c76eaa41403ef520ff11dcd6645b7d17
RedCloud79 <ghddns79@naver.com>
2023-03-30 10:18:15 +0900

    LED 깜빡이기

--- digitalwrite/digitalwrite.ino
+++ digitalwrite/digitalwrite.ino
@@ -3,13 +3,13 @@ const int Led = 13;
 void setup() {
   pinMode(Led, OUTPUT);
   //digitalWrite(Led, HIGH);
-  digitalWrite(Led, LOW);
+  //digitalWrite(Led, LOW);
 }
 
 void loop() {
-  // digitalWrite(Led, HIGH);
-  // delay(1000);
-  // digitalWrite(Led, LOW);
-  // delay(1000);
+  digitalWrite(Led, HIGH);
+  delay(100);
+  digitalWrite(Led, LOW);
+  delay(100);
 
 }
