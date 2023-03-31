bf51786d6df26f0535d654cc88c9fdb6ec6e1b86
RedCloud79 <ghddns79@naver.com>
2023-03-31 13:07:14 +0900

    Create lover_C.code.ino

---
+++ buzzer/lover_C.code/lover_C.code.ino
@@ -0,0 +1,229 @@
+#define C 262
+#define Cs 277
+#define D 294
+#define Ds 311
+#define E 330
+#define F 349
+#define Fs 370
+#define G 392
+#define Gs 415
+#define A 440
+#define As 466
+#define B 494
+#define hC 523
+#define hCs 554
+#define hD 587
+#define hDs 622
+#define hE 659
+#define hF 693
+#define hFs 740
+#define hG 784
+#define hGs 831
+#define hA 880
+#define hAs 932
+#define hB 988
+#define hhC 1047
+#define hhCs 1109
+#define hhD 1175
+#define hhDs 1245
+#define hhE 1319
+#define hhF 1397
+#define hhFs 1480
+#define hhG 1568
+#define hhGs 1661
+#define hhA 1760
+#define hhAs 1865
+#define hhB 1976
+#define hhhC 2093
+#define hhhCs 2217
+#define hhhD 2349
+#define hhhDs 2489
+#define hhhAs 3729
+#define hhhB 3951
+
+void setup() {
+  pinMode(5, OUTPUT);
+  //pinMode(6, OUTPUT);
+}
+
+void loop() {
+  delay(600);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(400);
+  tone(5, G, 1500);
+  delay(1000);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 1500);
+  delay(1000);
+
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 1000);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, C, 1500);
+  delay(1500);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+
+  tone(5, C, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, G, 1500);
+  delay(1000);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 1500);
+  delay(1000);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, C, 1500);
+  delay(1000);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, A, 400);
+  delay(300);
+
+  tone(5, G, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 800);
+  delay(800);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, A, 400);
+  delay(300);
+  tone(5, G, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+
+
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 800);
+  delay(800);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, A, 400);
+  delay(300);
+  tone(5, B, 400);
+  delay(300);
+  tone(5, A, 400);
+  delay(300);
+  tone(5, A, 400);
+  delay(300);
+  tone(5, B, 400);
+  delay(300);
+
+  tone(5, A, 400);
+  delay(300);
+  tone(5, G, 800);
+  delay(800);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, C, 400);
+  delay(300);
+  tone(5, A, 400);
+  delay(300);
+  tone(5, G, 400);
+  delay(300);
+  tone(5, E, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+
+  tone(5, C, 400);
+  delay(300);
+  tone(5, D, 400);
+  delay(300);
+  tone(5, C, 500);
+  delay(500);
+  
+
+}
