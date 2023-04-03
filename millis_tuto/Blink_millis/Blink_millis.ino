bf7fb78a4635a06fa64bd7d2cdcde10d801315db
RedCloud79 <ghddns79@naver.com>
2023-04-03 11:20:15 +0900

    다중작업하기 2

--- millis_tuto/Blink_millis/Blink_millis.ino
+++ millis_tuto/Blink_millis/Blink_millis.ino
@@ -1,21 +1,36 @@
-const int LED = 10;
+const int t1_LED = 13;
+int on_off = 0;
+
+const int t2_LED = 10;
 int t_high = 0;
 
-unsigned long t_prev = 0;
-const unsigned long t_delay = 10;
+unsigned long t1_prev = 0;
+const unsigned long t1_delay = 500;
+
+unsigned long t2_prev = 0;
+const unsigned long t2_delay = 4;
 
 void setup() {
-  
-  //pinMode(LED, OUTPUT);
+  pinMode(t1_LED, OUTPUT);
 }
 
 void loop() {
-  unsigned long t_now = millis();
-  if(t_now - t_prev >= t_delay){
-    t_prev = t_now;
+  unsigned long t1_now = millis();
+  if(t1_now - t1_prev >= t1_delay){
+    t1_prev = t1_now;
+
+    on_off++;
+    if(on_off>1) on_off = 0;
+    digitalWrite(t1_LED, on_off);
+  }
+
+  unsigned long t2_now = millis();
+  if(t2_now - t2_prev >= t2_delay){
+    t2_prev = t2_now;
+
     t_high++;
     if(t_high>255) t_high = 0;
-    digitalWrite(LED, t_high);
+    analogWrite(t2_LED, t_high);
   }
   
 }
