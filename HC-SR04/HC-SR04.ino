5136adae689d8c809db572b866fda4e9bf4c4078
RedCloud79 <ghddns79@naver.com>
2023-04-06 11:58:35 +0900

    10~90 까지 도~높은도 주파수 부저제어

--- HC-SR04/HC-SR04.ino
+++ HC-SR04/HC-SR04.ino
@@ -1,9 +1,18 @@
 #define C 262
+#define D 294
+#define E 330
+#define F 349
+#define G 392
+#define A 440
+#define B 494
+#define hC 523
 
 
 const int trigPin = 9;
 const int echoPin = 10;
-const int led = 7;
+const int led7 = 7;
+const int led6 = 6;
+const int led5 = 5;
 const int but = 3;
 
 float duration, distance;
@@ -11,7 +20,9 @@ float duration, distance;
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
   pinMode(but, INPUT);
   Serial.begin(9600);
 }
@@ -29,14 +40,53 @@ void loop() {
     Serial.print("Distance: ");
     Serial.println(distance);
     delay(100);
-    if (distance < 50){
-      digitalWrite(led, HIGH);
-      tone(11, C, 200);
-    }else{
-      digitalWrite(led, LOW);
-    }
+
+    switch(int(distance)) {
+      case 10 ... 19:
+          digitalWrite(led5, HIGH);
+          tone(11, C, 200);
+          break;
+      case 20 ... 29:
+          digitalWrite(led6, HIGH);
+          tone(11, D, 200);
+          break;
+      case 30 ... 39:
+          digitalWrite(led7, HIGH);
+          tone(11, E, 200);
+          break;
+      case 40 ... 49:
+          digitalWrite(led5, HIGH);
+          tone(11, F, 200);
+          break;
+      case 50 ... 59:
+          digitalWrite(led6, HIGH);
+          tone(11, G, 200);
+          break;
+      case 60 ... 69:
+          digitalWrite(led7, HIGH);
+          tone(11, A, 200);
+          break;
+      case 70 ... 79:
+          digitalWrite(led5, HIGH);
+          tone(11, B, 200);
+          break;
+      case 80 ... 89:
+          digitalWrite(led6, HIGH);
+          tone(11, hC, 200);
+          break;
+      default:
+          digitalWrite(led5, LOW);
+          digitalWrite(led6, LOW);
+          digitalWrite(led7, LOW);
+    } 
+    // if (distance < 50){
+    //   digitalWrite(led, HIGH);
+    //   tone(11, C, 200);
+    // }else{
+    //   digitalWrite(led, LOW);
+    // }
   }
-  // else(digitalRead(but) == HIGH){
-  //   exit(0);
-  // }
+  else{
+     exit(0);
+  }
 }
