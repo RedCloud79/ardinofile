4693460f93b96f9b2933a6cee6de4cabcc6241a0
RedCloud79 <ghddns79@naver.com>
2023-04-07 10:07:22 +0900

    조도센서 활용

---
+++ MH_sensor_series/switchCase/switchCase.ino
@@ -0,0 +1,45 @@
+const int sensorMin = 0;
+const int sensorMax = 600;
+int led[3] = {5,6,7};
+
+void setup() {
+  for(int i = 0; i < 3; i++){
+    pinMode(led[i], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorReading = analogRead(A1);
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+
+  switch (range) {
+    case 3:
+      Serial.println("dark");
+      for(int i = 0; i < 3; i++){
+      digitalWrite(led[i], LOW);
+      }
+      break;
+    case 2:
+      Serial.println("dim");
+      for(int i = 2; i < 3; i++){
+      digitalWrite(led[i], HIGH);
+      }
+      digitalWrite(led[1], LOW);
+      break;
+    case 1:
+      Serial.println("medium");
+      for(int i = 1; i < 3; i++){
+      digitalWrite(led[i], HIGH);
+      }
+      digitalWrite(led[0], LOW);
+      break;
+    case 0:
+      Serial.println("bright");
+      for(int i = 0; i < 3; i++){
+      digitalWrite(led[i], HIGH);
+      }
+      break;
+  }
+  delay(1);
+}
