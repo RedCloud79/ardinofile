ad87ca9610caf953e46e1ab7f421c619a8a62122
RedCloud79 <ghddns79@naver.com>
2023-04-07 13:11:13 +0900

    Update StateChangeDetection.ino

--- MH_sensor_series/StateChangeDetection/StateChangeDetection.ino
+++ MH_sensor_series/StateChangeDetection/StateChangeDetection.ino
@@ -1,3 +1,5 @@
+const int sensorMin = 0;
+const int sensorMax = 600;
 const int buttonPin = 3;
 int led[3] = {5,6,7};
 
@@ -6,15 +8,16 @@ int buttonState = 0;
 int lastButtonState = 0;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
   for(int i = 0; i < 3; i++){
     pinMode(led[i], OUTPUT);
   }
   Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
 }
 
-
 void loop() {
+  int sensorReading = analogRead(A1);
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
   buttonState = digitalRead(buttonPin);
 
   if (buttonState != lastButtonState) {
@@ -30,28 +33,41 @@ void loop() {
   }
   lastButtonState = buttonState;
 
-  int val = buttonPushCounter % 3;
-  
-  switch (val){
-    case 0:
-      break;
-    case 1:
-      digitalWrite(led[0], HIGH);
-      delay(500);
-      digitalWrite(led[0], LOW);
-      val = 0;
-      break;
-    case 2:
-      digitalWrite(led[1], HIGH);
-      delay(500);
-      digitalWrite(led[1], LOW);
-      val = 0;
-      break;
-    case 3:
-      digitalWrite(led[2], HIGH);
-      delay(500);
-      digitalWrite(led[2], LOW);
-      val = 0;
-      break;
+  if(buttonPushCounter % 2 == 0){
+    switch (range) {
+      case 3:
+        Serial.println("dark");
+        tone(11, 262, 200);
+        for(int i = 0; i < 3; i++){
+        digitalWrite(led[i], LOW);
+        }
+        break;
+      case 2:
+        Serial.println("dim");
+        tone(11, 294, 200);
+        for(int i = 2; i < 3; i++){
+        digitalWrite(led[i], HIGH);
+        }
+        digitalWrite(led[1], LOW);
+        break;
+      case 1:
+        Serial.println("medium");
+        for(int i = 1; i < 3; i++){
+        tone(11, 330, 200);
+        digitalWrite(led[i], HIGH);
+        }
+        digitalWrite(led[0], LOW);
+        break;
+      case 0:
+        Serial.println("bright");
+        tone(11, 349, 200);
+        for(int i = 0; i < 3; i++){
+        digitalWrite(led[i], HIGH);
+        }
+        break;
+    }
+  }else{
+    //break;
   }
+  delay(1);
 }
